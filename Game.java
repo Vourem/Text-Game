04214767220104a208feb6e0ce53402ae7452a43
Noah M <heyon99@gmail.com>
2017-01-17 13:19:10 -0500

    Updated text RPG

--- Game.java
+++ Game.java
@@ -10,6 +10,22 @@ public class Game {
         new Game();
     }
 
+    public static void slowPrintln(String text) {
+        slowPrintln(text, 40);
+    }
+
+    public static void slowPrintln(String text, int ms) {
+        for (char c : text.toCharArray()) {
+            System.out.print(c);
+            try {
+                Thread.sleep(ms);
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println();
+    }
+
     Scanner input;
 
     String[] monsterNames = {"Slime","Goblin","Steve","Annoying person"};
@@ -24,54 +40,72 @@ public class Game {
     public Game() {
         input = new Scanner(System.in);
 
-        while (pHealth > 0) continue; {
-        System.out.println("\nYou awaken in a desolate field, with nothing but grass and a handful of trees scattered around you.\n");
+        ///while (pHealth > 0) {
+        slowPrintln("\nYou awaken in a desolate field, with nothing but grass and a handful of trees scattered around you.\n");
         System.out.println("---------------------------------\n");
-        System.out.println("What do you do?");
+        slowPrintln("What do?");
 
-        char action = input.nextLine().toLowerCase().charAt(0);
+        System.out.print("> ");
+        String thing = input.nextLine();
+        char action = thing.toLowerCase().charAt(0);
 
         switch (action) {
 
             case 'l':
-                System.out.println("You look around. Upon further investigation you notice what looks like a small house to the east.");
-                break;
-            case 'r':
-                System.out.println("You have no supplies other than simple clothes and a pouch with a loaf of bread.\n");
+                slowPrintln("You look around. Upon further investigation you notice what looks like a small house to the east.\nWhat next?");
+                System.out.print("> ");
+                thing = input.nextLine();
+                action = thing.toLowerCase().charAt(0);
+                switch (action) {
+                    case 'g':
+                        slowPrintln("Upon arriving at the structure, you see that it is a small house. You stop before reaching what looks like the main door.");
+                        break;
+                    case 'c':
+                        slowPrintln("Upon arriving at the structure, you see that it is a small house. You stop before reaching what looks like the main door.");
+                    case 'q':
+                        System.exit(0);
+                    default:
+                        slowPrintln("You " + thing + ".");
+                }
                 break;
             case 'h':
-                System.out.println("Your options are \n[L]ook around\n[R]est\n[H]elp");
+                slowPrintln("Your options are \n[L]ook around\n[R]est\n[H]elp");
+                break;
+            case 'q':
+                System.exit(0);
             default:
-                System.out.println("You have no supplies other than simple clothes and a pouch with a loaf of bread.\n");
+                slowPrintln("You have no supplies other than simple clothes and a pouch with a loaf of bread, but you do manage to " + thing + ".\n");
                 break;
             
-}
+        }
         if (mHealth <= 0) {
-            ///xp += mHealth/=2;
             mHealth = 20;
             currentMonster = monsterNames[(int)Math.floor(Math.random()*monsterNames.length)];
-            ///System.out.println("You have gained " + xp + " XP.");
-            System.out.println(currentMonster + " approaches!");
+            slowPrintln(currentMonster + " approaches!", 20);
 
 
-        System.out.println("What's your plan, buddy?\n[A]ttack\n[H]eal\n[N]egotiate\n[R]un");
+       slowPrintln("What's your plan, buddy?\n[A]ttack\n[H]eal\n[N]egotiate\n[R]un");
+        System.out.print("> ");
+        thing = input.nextLine();
+        action = thing.toLowerCase().charAt(0);
 
         switch (action) {
 
             case 'a':
-                System.out.println("You attack the " + currentMonster + "!!");
+                slowPrintln("You attack the " + currentMonster + "!!");
                 mHealth -= 20;
-                System.out.println(currentMonster + " is now at " + mHealth + " health. Good job, for a newbie.");
+                slowPrintln(currentMonster + " is now at " + mHealth + " health. Good job, for a newbie.");
                 break;
             case 'h':
                 pHealth += 3;
-                System.out.println("You take a swig-skey of whiskey. All your problems fly out the window!\nYou are now at " + pHealth + " health.");
+                slowPrintln("You take a swig-skey of whiskey. All your problems fly out the window!\nYou are now at " + pHealth + " health.");
                 break;
             case 'q':
                 System.exit(0);
+            default:
+                System.out.println("You " + thing + ".");
             }
         
     }
-}
 }
 }
